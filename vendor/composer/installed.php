45649e18b9d2269a6c0e15559a8394e67f89edc2
mppankov <pankov.mp@yandex.ru>
2024-02-12 14:21:21 +0300

    initiall commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mppankov/blog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cb7dde05f8f6d4fc63c161b6f39ff126d8795839',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mppankov/blog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cb7dde05f8f6d4fc63c161b6f39ff126d8795839',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
